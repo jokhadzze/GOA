ec141d94d89da10b9edcba30021066cebf1575c9
jokhadzze <jokhadze.ninka@gmail.com>
2024-03-04 21:50:36 +0400

    update day 5

---
+++ day5/hw.py
@@ -0,0 +1,20 @@
+movie = "fight club"
+sport = "kickbox"
+healer = "gym"
+price = 56.89
+amount_of_students = 170
+amount_of_my_pets = 16
+height = 180
+bench_pr = "unknown"
+pi = 3.14
+song = "hotel"
+print(type(movie))
+print(type(sport))
+print(type(healer))
+print(type(price))
+print(type(amount_of_students))
+print(type(amount_of_my_pets))
+print(type(height))
+print(type(bench_pr))
+print(type(pi))
+print(type(song))
