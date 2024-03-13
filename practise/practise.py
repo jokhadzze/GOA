8ce7ff57cb5cf4ef37f6251ab233b1106cd740ab
jokhadzze <160392748+jokhadzze@users.noreply.github.com>
2024-03-13 14:45:40 +0400

    Delete practise directory

--- practise/practise.py
+++
@@ -1,7 +0,0 @@
-name = input("what is your name: ")
-lastname  = input("what is your last name: ")
-salary = int(input("what is your salary: "))
-
-print(f"my name is {name} {lastname} and my salary is {salary}")
-
-#format string გვაძველს საშუალებას ჩავსვათ ნებისმიერი მნიშვნელობა სტრინგში
