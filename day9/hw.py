ba3b6c40662fb72b69fa3f2008b7bdf7f63553ec
jokhadzze <jokhadze.ninka@gmail.com>
2024-03-13 21:34:07 +0400

    update day 9

---
+++ day9/hw.py
@@ -0,0 +1,32 @@
+#1
+
+number = int(input("enter number: "))   
+
+print(number % 2==0 and number % 4==0)
+
+#2
+
+print(5>3)
+print(78<9)
+print(12>=21)
+print(34<=34)
+print(4==4)
+print(123!=2)
+print(5.6>9)
+print(0.7==0.07)
+print(89!=89)
+print(19>=6)
+
+
+#3
+
+print(4==5 and 9==9)
+print(9>=12 and 9!=3)
+print(3<4.2 and 4.2>3)
+print(89!=89 and 90<91)
+print(78>0 and 67<=90)
+print(1121>1122 and 78==78)
+print(80>=79 and 12!=13)
+print(69<100 and 90>=89)
+print(12!=12 and 12==12)
+print(1>101 and 5==5)
