359af04930633a08bdc15c018f013569c4a0d961
jokhadzze <jokhadze.ninka@gmail.com>
2024-03-13 17:38:10 +0400

    update day 8

---
+++ day8/hw.py
@@ -0,0 +1,8 @@
+name = input("please, enter your name: ")
+password = input("pleas, enter your password: ")
+repeat_password = input("please, repeat the password: ")
+
+if password == repeat_password:
+    print("თქვენ წარმატებით გაიარეთ რეგისტრაცია")
+else:
+    print("პაროლი არასწორია")
--- practise/practise2.py
+++ practise/practise2.py
@@ -1,1 +1,8 @@
-print(range(40))
+# for i in range(3):
+#     print(i < 1)
+
+age = int(input("how old are you: "))
+if age >= 18:
+    print("kai roja chanxar")
+else:
+    print("shkolnikoooo")
