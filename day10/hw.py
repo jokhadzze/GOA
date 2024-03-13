83fc7a428657f4eaa002a23c0daff23d91ac01ea
jokhadzze <jokhadze.ninka@gmail.com>
2024-03-14 01:06:48 +0400

    update day 10

---
+++ day10/hw.py
@@ -0,0 +1,29 @@
+#1
+
+programming_languages = ["python", "java script", "css", "html", "c++"]
+print(programming_languages)
+print(programming_languages[4])
+
+#2
+
+random = ["gym", "140", "19.1", "True"]
+print(random[0])
+print(random[1])
+print(random[2])
+print(random[3])
+
+#3
+
+nums = ["4", "8", "12", "16", "20"]
+print(nums[4])
+
+#4
+
+numbers = ["31", "33", "35", "37", "39", "41", "43", "45", "47", "49"]
+print(int(numbers[7]) + int(numbers[8]) + int(numbers[9]))
+
+#5
+
+print(numbers[1])
+print(numbers[4])
+print(numbers[7])
