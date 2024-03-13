11e809316fd7a58443bb2c29a2b7abb0d2f496b0
jokhadzze <jokhadze.ninka@gmail.com>
2024-03-13 14:03:08 +0400

    update day7

---
+++ day7/hw.py
@@ -0,0 +1,22 @@
+for i in range(2,10,2):
+   print(i)
+
+
+for i in "gym":
+    print(i)
+
+
+number = 1
+while number <= 20:
+    print(number)
+    number +=1
+
+
+
+result = 0
+i = 0
+while i <= 5:
+    result = result + i
+    i = i + 1
+    
+print(result) 
---
+++ practise/practise.py
@@ -0,0 +1,7 @@
+name = input("what is your name: ")
+lastname  = input("what is your last name: ")
+salary = int(input("what is your salary: "))
+
+print(f"my name is {name} {lastname} and my salary is {salary}")
+
+#format string გვაძველს საშუალებას ჩავსვათ ნებისმიერი მნიშვნელობა სტრინგში
