1ed256caae2ba717a4cf693fc7fc82ecf705ed64
jokhadzze <jokhadze.ninka@gmail.com>
2024-03-14 01:44:26 +0400

    update day 10

--- day10/hw.py
+++ day10/hw.py
@@ -6,7 +6,7 @@ print(programming_languages[4])
 
 #2
 
-random = ["gym", "140", "19.1", "True"]
+random = ["gym", 140, 19.1, True]
 print(random[0])
 print(random[1])
 print(random[2])
@@ -14,12 +14,12 @@ print(random[3])
 
 #3
 
-nums = ["4", "8", "12", "16", "20"]
+nums = [4, 8, 12, 16, 20]
 print(nums[4])
 
 #4
 
-numbers = ["31", "33", "35", "37", "39", "41", "43", "45", "47", "49"]
+numbers = [31, 33, 35, 37, 39, 41, 43, 45, 47, 49]
 print(int(numbers[7]) + int(numbers[8]) + int(numbers[9]))
 
 #5
