8147a0f5399a571e82eb09213e4f89e0e496e167
jokhadzze <jokhadze.ninka@gmail.com>
2024-03-14 15:46:23 +0400

    update day 11

---
+++ day11/hw.py
@@ -0,0 +1,33 @@
+#1
+
+nums = [8, 10, 55, 89, 1, 0, 19, 21, 13, 16]
+print(nums[5] * nums [9])
+print(nums[3] / nums[7])
+print(nums[1] + nums[5])
+print(nums[0] - nums[2])
+print(nums[7] // nums[3])
+print(nums[2] * nums[7])
+print(nums[4] - nums[5])
+print(nums[0] / nums[4])
+print(nums[9] + nums[2])
+
+#2
+
+gym_inventory = ["towel", "water bottle", "deodorant", "preworkout"]
+print(gym_inventory[0])
+print(gym_inventory[1])
+print(gym_inventory[2])
+print(gym_inventory[3])
+
+gym_inventory[3] = "whey protein"
+gym_inventory[1] = "jump rope"
+
+print(gym_inventory[3])
+print(gym_inventory[1])
+
+
+#3
+
+arr = ['money', 'ps5', 'gta6', 'unlimited freedom']
+choose = int((input("enter index: ")))
+print(arr[choose])
