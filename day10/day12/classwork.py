55ff39cdb1cbf960667b1dbd8915ef1def7e61d3
jokhadzze <jokhadze.ninka@gmail.com>
2024-06-02 14:26:57 +0400

    commit

---
+++ day10/day12/classwork.py
@@ -0,0 +1,15 @@
+activity = "cardio"
+
+gym_inventory = ["towel", "water bottle", "deodorant", "preworkout"]
+
+print(activity[0 : 2])
+
+print(gym_inventory[0 : 3])
+
+print(activity[-1 : -3])
+
+print(gym_inventory[-2 : -4])
+
+
+
+
